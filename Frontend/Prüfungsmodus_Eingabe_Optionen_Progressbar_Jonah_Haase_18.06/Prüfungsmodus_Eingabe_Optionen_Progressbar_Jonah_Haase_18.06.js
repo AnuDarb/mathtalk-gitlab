edda1db15a585299b8e8a128fd5afba55fc4cfc9
Jonah Haase <haase@uni-hildesheim.de>
2025-06-18 06:22:27 +0000

    Upload New File

---
+++ Frontend/Prüfungsmodus_Eingabe_Optionen_Progressbar_Jonah_Haase_18.06/Prüfungsmodus_Eingabe_Optionen_Progressbar_Jonah_Haase_18.06.js
@@ -0,0 +1,8 @@
+function toggleOptions() {
+  const panel = document.getElementById('optionsPanel');
+  panel.classList.toggle('visible');
+}
+
+function saveOptions() {
+  alert("Optionen gespeichert!");
+}
