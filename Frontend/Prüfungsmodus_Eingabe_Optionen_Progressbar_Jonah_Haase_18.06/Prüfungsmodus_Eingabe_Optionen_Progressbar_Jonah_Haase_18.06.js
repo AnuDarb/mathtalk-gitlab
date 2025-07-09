b0e6bc178b119f611de56f4fa4867fea3068a231
AnuDarb <darbinyan@uni-hildesheim.de>
2025-07-09 14:42:44 +0200

    Delete Frontend/Prüfungsmodus_Eingabe_Optionen_Progressbar_Jonah_Haase_18.06 directory

--- Frontend/Prüfungsmodus_Eingabe_Optionen_Progressbar_Jonah_Haase_18.06/Prüfungsmodus_Eingabe_Optionen_Progressbar_Jonah_Haase_18.06.js
+++
@@ -1,8 +0,0 @@
-function toggleOptions() {
-  const panel = document.getElementById('optionsPanel');
-  panel.classList.toggle('visible');
-}
-
-function saveOptions() {
-  alert("Optionen gespeichert!");
-}
